4f0a903dbe813712a9ae8f0a9ad11841f6ab60cf
aveek <aveek.n@icarasia.com>
2023-02-09 12:36:56 +0800

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Aveek on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
